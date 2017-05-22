94e03004defc89d7942b1b6d9b6565d84514b40f
Philip Kamenarsky <p.kamenarsky@gmail.com>
2017-05-22 16:24:02 +0200

    WIP

--- src/Act.purs
+++ src/Act.purs
@@ -2,24 +2,27 @@ module Act where
 
 import Control.Monad.Eff
 import Data.Lens
-import Data.Maybe (fromJust)
 import Data.Tuple
 import Unsafe.Coerce
-import Partial.Unsafe (unsafePartial)
 import Prelude
 import DOM as D
+import DOM.Node.Types as D
+import React as R
+import React.DOM as R
+import React.DOM.Props as P
 import DOM.HTML (window)
 import DOM.HTML.Types (htmlDocumentToDocument)
 import DOM.HTML.Window (document)
 import DOM.Node.NonElementParentNode (getElementById)
 import DOM.Node.Types (ElementId(..), documentToNonElementParentNode)
-import DOM.Node.Types as D
-import React as R
-import React.DOM as R
-import React.DOM.Props as P
+import Data.Maybe (fromJust)
+import Partial.Unsafe (unsafeCrashWith, unsafePartial)
 import React (transformState)
 import React.DOM.Props (onClick)
 import ReactDOM (render)
+
+undefined :: forall a. a
+undefined = unsafeCoerce unit
 
 data Effect st' st =
     Pure (st -> st)
@@ -41,15 +44,28 @@ type Component st = forall pst. ChildComponent pst st
 div :: forall pst st. Array (Props pst st) -> Array (Element pst st) -> Element pst st
 div = R.div
 
-counter :: Component Int
+button :: Component Boolean
+button =
+  { render: \st dispatch -> div [ P.onMouseDown \e -> dispatch $ Pure not ] [ R.text ("button: " <> show st) ]
+  }
+
+counter :: Component (Tuple String Boolean)
 counter =
-  { render: \st dispatch -> div [ P.onClick \e -> dispatch $ Pure (_ + 1) ] [ R.text ("counter: " <> show st) ]
+  { render: \st dispatch ->
+     div
+       [ P.onMouseUp \e -> dispatch $ Pure (\(Tuple s b) -> Tuple (s <> "QM") b) ]
+       [ R.text ("state: " <> show st)
+       , zoom _2 dispatch st button
+       ]
   }
 
 mapEffect :: forall st st' st''. Lens' st' st -> Effect st' st -> Effect st'' st'
 mapEffect lns (Pure f) = Pure (over lns f)
 mapEffect lns (GetHTTP url eff) = GetHTTP url \r -> (mapEffect lns (eff r))
 mapEffect lns (Parent f) = Pure f
+
+zoom :: forall ppst pst st. Lens' pst st -> (Effect ppst pst -> Handler pst) -> pst -> ChildComponent pst st -> Element ppst pst
+zoom lns dispatch pst cmp = cmp.render (view lns pst) \e -> dispatch (mapEffect lns e)
 
 mkSpec :: forall eff st. st -> Component st -> R.ReactSpec Unit st eff
 mkSpec st cmp = R.spec st \this -> do
@@ -61,7 +77,7 @@ mkSpec st cmp = R.spec st \this -> do
 
 main :: forall eff. Eff (dom :: D.DOM | eff) Unit
 main = void (elm' >>= render ui)
-  where ui = R.createFactory (R.createClass (mkSpec 0 counter)) unit
+  where ui = R.createFactory (R.createClass (mkSpec (Tuple "Quantum" false) counter)) unit
 
         elm' :: Eff (dom :: D.DOM | eff) D.Element
         elm' = do
