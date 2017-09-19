48bfeba1a1c0ff388d639147fcb3380b9f8e0fac
Philip Kamenarsky <p.kamenarsky@gmail.com>
2017-09-19 16:37:52 +0200

    WIP

--- src/Match.purs
+++ src/Match.purs
@@ -2,6 +2,7 @@ module Match where
 
 import Prelude
 import Data.Array
+import Data.List
 import Data.Maybe
 import Data.Tuple
 import Data.Tuple.Nested
@@ -21,6 +22,9 @@ derive instance genericVar :: Generic Var
 instance eqVar :: Eq Var where
   eq = gEq
 
+instance ordVar :: Ord Var where
+  compare = gCompare
+
 newtype Const = Const String
 
 derive instance genericConst :: Generic Const
@@ -31,7 +35,7 @@ instance eqConst :: Eq Const where
 data RType = RVar Var
            | RConst Const
            | RApp RType RType
-           | RFun (Array RType) RType
+           | RFun (List RType) RType
 
 derive instance genericRType :: Generic RType
 
@@ -50,7 +54,7 @@ data RTransform = REqual
                 | RMap Const Const
                 | RCoerce RCoercion
                 | RNoMatch
-                | RTFun (Array RTransform)
+                | RTFun (List RTransform) RTransform
 
 derive instance genericRTransform :: Generic RTransform
 
@@ -72,6 +76,16 @@ specifyType (RVar t) (RConst u) = RSpecify t u
 specifyType (RApp t x) (RApp u y)
   | t == u    = specifyType x y
   | otherwise = RNoMatch
+-- TODO: apply context to result type
+specifyType (RFun args x) (RFun args' x') = RTFun (match M.empty args args' Nil) (specifyType x x')
+  where
+    match :: M.Map Var Const -> List RType -> List RType -> List RTransform -> List RTransform
+    match ctx (Cons (RVar a) as) b'@(Cons (RConst b) bs) tr
+      | Just v <- M.lookup a ctx
+                  = match ctx (Cons (RConst v) as) b' tr
+      | otherwise = match (M.insert a b ctx) as bs (Cons (RSpecify a b) tr) 
+    match ctx (Cons a as) (Cons b bs) tr = match ctx as bs (Cons (specifyType a b) tr) 
+    match _ _ _ tr = tr
 specifyType _ _ = RNoMatch
 
 type ArgList = Array RType
@@ -80,4 +94,4 @@ specifyFun :: RType -> ArgList -> Array RType -> Array (RTransform /\ ArgList)
 specifyFun = undefined
 
 testSpecify :: RTransform
-testSpecify = specifyType (RApp (RConst (Const "array")) (RVar (Var "a"))) (RApp (RConst (Const "list")) (RConst (Const "person")))
+testSpecify = specifyType (RApp (RConst (Const "array")) (RVar (Var "a"))) (RApp (RConst (Const "array")) (RConst (Const "person")))
