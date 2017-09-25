68d00b1e6b998e67a71657d7bf20d5a6c87b2c13
Philip Kamenarsky <p.kamenarsky@gmail.com>
2017-09-25 18:28:21 +0200

    WIP

--- src/Act.purs
+++ src/Act.purs
@@ -540,18 +540,18 @@ typeComponent r ss t = typeComponent' t r ss t
   where
     typeComponent' :: RType -> Rect -> Lens' AppState (L.List Substitution) -> RType -> Component eff AppState
     typeComponent' tt bounds@(bx × by × bw × bh) substs rtype
-      | Just (incoming × children) <- extract rtype = g [] $ concat
+      | Just (incoming × children) <- extract rtype = state \st -> g [] $ concat
           [ [ uirect bounds ]
           , inc (A.fromFoldable incoming)
-          , [ snd $ snch unit ]
+          , [ snd $ snch st ]
           ]
       where
         inc :: Array (Label × RType) -> Array (Component eff AppState)
         inc incoming = flip map (indexedRange incoming) \(i × l × t) ->
           uicircle (bx - gap × by + (tn i * gap)) (UILabelLeft $ show t)
     
-        child :: Rect -> Maybe Substitution × Label × RType -> Component eff AppState
-        child bounds@(ix × iy × _ × _) (s × l × t@(RFun args _))
+        child :: AppState -> Rect -> Maybe Substitution × Label × RType -> Component eff AppState
+        child st bounds@(ix × iy × _ × _) (s × l × t@(RFun args _))
           | isHOC t = g [] $ concat
             [ [ uirect bounds ]
             , case s of
@@ -559,25 +559,25 @@ typeComponent r ss t = typeComponent' t r ss t
                   Just t'   -> [ typeComponent' tt shrunkBounds undefined t' ]
                   otherwise -> [ uirectDashed shrunkBounds ]
                 otherwise         -> [ uirectDashed shrunkBounds ]
-            , ext (ix × (iy + gap)) (A.fromFoldable args)
+            , ext st (ix × (iy + gap)) (A.fromFoldable args)
             ]
             where
               shrunkBounds = shrink ((7.0 * gap) × (1.0 * gap) × gap × gap) bounds
           | otherwise = undefined
-        child _ _ = g [] []
+        child _ _ _ = g [] []
 
         -- TODO: very inefficient
-        snch :: Unit -> ((Rect -> Maybe (Maybe Substitution × Label × RType)) × Component eff AppState)
-        snch _ = subdivide' bounds (shrink ((7.0 * gap) × (1.0 * gap) × gap × gap)) (A.fromFoldable $ zipMaybe undefined children) child
+        snch :: AppState -> ((Rect -> Maybe (Maybe Substitution × Label × RType)) × Component eff AppState)
+        snch st = subdivide' bounds (shrink ((7.0 * gap) × (1.0 * gap) × gap × gap)) (A.fromFoldable $ zipMaybe (st ^. substs) children) (child st)
 
-        ext :: Vec -> Array (Label × RType) -> Array (Component eff AppState)
-        ext (ox × oy) external = map ext' (indexedRange external)
+        ext :: AppState -> Vec -> Array (Label × RType) -> Array (Component eff AppState)
+        ext st (ox × oy) external = map ext' (indexedRange external)
           where
             ext' (i × l × t@(RFun _ (RConst (Const "Component")))) = g
               [ onMouseDrag \e -> case e of
                   DragStart e -> modify \st -> st { dragState = Just $ DragHOC { hoc: t, label: l, pos: meToV e } }
                   DragMove  e -> modify \st -> st { dragState = Just $ DragHOC { hoc: t, label: l, pos: meToV e } }
-                  DragEnd   e -> modify \st -> st { dragState = Nothing, debug = show $ map snd $ (fst (snch unit)) (e.pageX × e.pageY × 200.0 × 100.0) }
+                  DragEnd   e -> modify \st -> st { dragState = Nothing, debug = show $ map snd $ (fst (snch st)) (e.pageX × e.pageY × 200.0 × 100.0) }
               ]
               [ uicircle (ox + gap × oy + (tn i * gap)) (UILabelRight "HOC") ]
             ext' (i × l × t) = uicircle (ox + gap × oy + (tn i * gap)) (UILabelRight $ show t)
