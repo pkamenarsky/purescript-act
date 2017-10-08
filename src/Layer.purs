a14b0e52b3dfd8b4b4bb8fdc99a2546a3ba94468
Philip Kamenarsky <p.kamenarsky@gmail.com>
2017-10-08 15:39:23 +0200

    WIP

--- src/Layer.purs
+++ src/Layer.purs
@@ -2,6 +2,7 @@ module Layer where
 
 import Component
 
+import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import Prelude hiding (div)
 import Undefined (undefined)
@@ -12,61 +13,54 @@ infixr 6 type Tuple as ×
 
 type Rect  = Number × Number × Number × Number
 
-data Layer a = Layer Rect a (Array (Layer a))
+data Layer b a = Layer Rect b (Layer b a)
+               | Append (∀ c. Layer b c) (Layer b a)
+               | Empty a 
 
-layer :: forall a. Rect -> a -> Array (Layer a) -> Layer a
-layer = Layer
+mapLayerRect :: ∀ b a. (Rect -> Rect) -> Layer b a -> Layer b a
+mapLayerRect f (Empty a) = Empty a
+mapLayerRect f (Layer rect b children) = Layer (f rect) b (mapLayerRect f children)
+mapLayerRect f (Append x y) = Append (mapLayerRect f x) (mapLayerRect f y)
 
-data LayerM b a = LayerM Rect b (LayerM b a) | Empty a | Append (forall c. LayerM b c) (LayerM b a)
+type LayerCmp eff st a = Layer (Array (Component eff st) -> Component eff st) a
 
-mapLayerMRect :: forall b a. (Rect -> Rect) -> LayerM b a -> LayerM b a
-mapLayerMRect f (Empty a) = Empty a
-mapLayerMRect f (LayerM rect b children) = LayerM (f rect) b (mapLayerMRect f children)
-mapLayerMRect f (Append x y) = Append (mapLayerMRect f x) (mapLayerMRect f y)
+renderLayer :: ∀ eff st. LayerCmp eff st Unit -> Array (Component eff st)
+renderLayer (Empty _) = []
+renderLayer (Append x y) = renderLayer x <> renderLayer y
+renderLayer (Layer rect b children) = [ b (renderLayer children) ]
 
-instance functorLayer :: Functor (LayerM b) where
-  map = undefined
+instance functorLayer :: Functor (Layer b) where
+  map f (Layer rect b children) = Layer rect b (map f children)
+  map f (Append x y) = Append x (map f y)
+  map f (Empty a) = Empty (f a)
 
-instance applyLayer :: Apply (LayerM b) where
+instance applyLayer :: Apply (Layer b) where
   apply = undefined
 
-instance applicativeLayer :: Applicative (LayerM b) where
+instance applicativeLayer :: Applicative (Layer b) where
   pure = Empty
 
-layerValue :: forall b a. LayerM b a -> a
+layerValue :: ∀ b a. Layer b a -> a
 layerValue = undefined
 
-instance bindLayer :: Bind (LayerM b) where
+instance bindLayer :: Bind (Layer b) where
   bind m f = Append (unsafeCoerce m) (f (layerValue m))
-
-type LayerCmpM eff st a = LayerM (Array (Component eff st) -> Component eff st) a
-
-data LayerRef
 
 mvrect :: Rect -> Rect -> Rect
 mvrect = undefined
 
-layerM :: forall b a. Rect -> b -> LayerM b a -> LayerM b a
-layerM rect b children = LayerM rect b (mapLayerMRect (mvrect rect) children)
+layer :: ∀ b a. Rect -> b -> Layer b a -> Layer b a
+layer rect b children = Layer rect b (mapLayerRect (mvrect rect) children)
 
-rect :: forall eff st a. LayerCmpM eff st a -> LayerCmpM eff st Rect
-rect _ = undefined
+rect :: ∀ eff st a. LayerCmp eff st a -> Maybe Rect
+rect (Layer rect _ _) = Just rect
+rect _ = Nothing
 
-testM :: forall eff st. LayerCmpM eff st (LayerRef × LayerRef)
-testM = layerM (0.0 × 0.0 × 0.0 × 0.0) (div []) $ do
-  a <- layerM (0.0 × 0.0 × 0.0 × 0.0) (div []) undefined
-  b <- layerM (0.0 × 0.0 × 0.0 × 0.0) (div []) undefined
+intersect :: ∀ eff st a. Rect -> LayerCmp eff st a -> Maybe (LayerCmp eff st a)
+intersect = undefined
+
+testM :: ∀ eff st. LayerCmp eff st (LayerCmp eff st Unit × LayerCmp eff st Unit)
+testM = Layer (0.0 × 0.0 × 0.0 × 0.0) (div []) $ do
+  a <- Layer (0.0 × 0.0 × 0.0 × 0.0) (div []) undefined
+  b <- Layer (0.0 × 0.0 × 0.0 × 0.0) (div []) undefined
   pure (a × b)
-
---------------------------------------------------------------------------------
-
-type LayerCmp eff st = Layer (Array (Component eff st) -> Component eff st)
-
-test :: forall eff st. LayerCmp eff st
-test = layer (0.0 × 0.0 × 0.0 × 0.0) (div [])
-  [ layer (0.0 × 0.0 × 0.0 × 0.0) (const $ text "bla") []
-  , layer (0.0 × 0.0 × 0.0 × 0.0) (div []) []
-  ]
-
-cmpFromLayerCmp :: forall eff st. LayerCmp eff st -> Component eff st
-cmpFromLayerCmp (Layer _ a children) = a (map cmpFromLayerCmp children)
